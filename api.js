93b8163318fa93b1b8f437055c10eac2dbea01f2
James Wang <jameswang9909@hotmail.com>
2017-11-27 16:16:39 -0600

    Prevent divide by zero operations

--- api.js
+++ api.js
@@ -93,7 +93,12 @@ function calculateRepoStupidity(repo, callback) {
         var stars = result["stargazers_count"];
         var forks = result["forks_count"];
         var contributors = result["contributors"];
-        var stupidity = (((forks - contributors) / stars) * 100); // Yes, I'm aware that if stars == 0 then the stupidity is Infinity.
+        var stupidity;
+        if (stars === 0) {
+            stupidity = 0;
+        } else {
+            stupidity = (((forks - contributors) / stars) * 100);
+        }
         stupidity = stupidity.toFixed(2);
 
         if (stupidity < 0) {
