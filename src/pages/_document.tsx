8f5b102e793f58faf6ed48c1f38dbc8918f0cc0c
James Wang <jameswang9909@hotmail.com>
2019-03-21 22:31:49 -0500

    Add _app and _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript, NextDocumentContext } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: NextDocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
